245738b0450c27e5d395e08661949c95f9458cdb
d-rue <daniel.rueck@web.de>
2023-06-06 23:13:53 +0200

    Splitted SecurityFilterChain for api and h2 console

--- src/main/java/de/drue/UserApp/Config/SecurityConfig.java
+++ src/main/java/de/drue/UserApp/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.drue.UserApp.Config;
 import de.drue.UserApp.Service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,18 +30,29 @@ public class SecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-        http
+    @Order(1)
+    SecurityFilterChain h2ConsoleSecurityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher(AntPathRequestMatcher.antMatcher("/h2/**"))
                 .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2/**")))
                 .authorizeHttpRequests(authRequest -> authRequest
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/h2/**")).permitAll()
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher("/**")
+                .authorizeHttpRequests(authRequest -> authRequest
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(userDetailsServiceImpl)
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .oauth2Login(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
-
-        return http.build();
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 }
