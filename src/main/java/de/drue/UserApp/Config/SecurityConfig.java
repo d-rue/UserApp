f1cd846e307f59811da0b075657f30260dd201f8
d-rue <daniel.rueck@web.de>
2023-06-06 15:58:19 +0200

    Added SecurityFilterChain

--- src/main/java/de/drue/UserApp/Config/SecurityConfig.java
+++ src/main/java/de/drue/UserApp/Config/SecurityConfig.java
@@ -2,9 +2,13 @@ package de.drue.UserApp.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +16,21 @@ public class SecurityConfig {
     @Bean
     public static PasswordEncoder encode() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public static SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/*").permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/h2/**").permitAll()
+
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        return http.build();
     }
 }
