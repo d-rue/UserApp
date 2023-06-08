1f719cf53e93e3cd17103b659384b55edbb59ab9
d-rue <daniel.rueck@web.de>
2023-06-08 15:28:30 +0200

    Changed Controller return type to ResponseEntity

--- src/main/java/de/drue/UserApp/Controller/HomeController.java
+++ src/main/java/de/drue/UserApp/Controller/HomeController.java
@@ -1,5 +1,7 @@
 package de.drue.UserApp.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,19 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public String home(){
-        return "Home";
+    public ResponseEntity<String> home(){
+        return new ResponseEntity<>("Home", HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/user")
-    public String user(){
-        return "Hallo User";
+    public ResponseEntity<String> user(){
+        return new ResponseEntity<>("Hallo User", HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/admin")
-    public String admin(){
-        return "Hallo Admin";
+    public ResponseEntity<String> admin(){
+        return new ResponseEntity<>("Hallo Admin", HttpStatus.OK);
     }
 }
