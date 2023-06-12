6199290f872ee39f692cdf0771b55ac3cb6b76ac
d-rue <daniel.rueck@web.de>
2023-06-12 22:05:31 +0200

    Added Authentication.getName() to HomeController

--- src/main/java/de/drue/UserApp/Controller/HomeController.java
+++ src/main/java/de/drue/UserApp/Controller/HomeController.java
@@ -3,25 +3,26 @@ package de.drue.UserApp.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public ResponseEntity<String> home(){
+    public ResponseEntity<String> home() {
         return new ResponseEntity<>("Home", HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/user")
-    public ResponseEntity<String> user(){
-        return new ResponseEntity<>("Hallo User", HttpStatus.OK);
+    public ResponseEntity<String> user(final Authentication authentication){
+        return new ResponseEntity<>("Hallo " + authentication.getName(), HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/admin")
-    public ResponseEntity<String> admin(){
-        return new ResponseEntity<>("Hallo Admin", HttpStatus.OK);
+    public ResponseEntity<String> admin(final Authentication authentication){
+        return new ResponseEntity<>("Hallo " + authentication.getName(), HttpStatus.OK);
     }
 }
